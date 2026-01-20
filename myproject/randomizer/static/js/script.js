99175679695f7f90d79c71c777637a2b83d966df
Tervasroso <tuokivilahti@gmail.com>
2026-01-20 09:59:01 +0200

    html:ää ja muotoilua lisätty

--- myproject/randomizer/static/js/script.js
+++ myproject/randomizer/static/js/script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Page loaded in scripts is running");
 });
+
+var gameMode = document.getElementById("gameMode");
+
+// function renderGameMode() {
+//   gameMode.innerHTML = ''
+// }
