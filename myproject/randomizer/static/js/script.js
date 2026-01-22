db572f47c31380f0e07f8f74b0802de7a07faf5d
Tervasroso <tuokivilahti@gmail.com>
2026-01-22 09:52:49 +0200

    index.html updated, row selections are now in objects

--- myproject/randomizer/static/js/script.js
+++ myproject/randomizer/static/js/script.js
@@ -1,9 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Page loaded in scripts is running");
+  console.log("Page loaded");
 });
 
-var gameMode = document.getElementById("gameMode");
+ov = 0;
+tv = 0;
 
-// function renderGameMode() {
-//   gameMode.innerHTML = ''
-// }
+var rows = [
+  { group: "A", count: 0 },
+  { group: "B", count: 0 },
+  { group: "C", count: 0 },
+  { group: "D", count: 0 },
+  { group: "E", count: 0 },
+  { group: "F", count: 0 },
+  { group: "G", count: 0 },
+  { group: "H", count: 0 },
+  { group: "I", count: 0 },
+  { group: "J", count: 0 },
+  { group: "K", count: 0 },
+  { group: "L", count: 0 },
+  { group: "M", count: 0 },
+];
+
+function ifChecked(id, name) {
+  console.log(name);
+  if (document.getElementById(id).checked) {
+    for (let i = 0; i < rows.length - 1; i++) {
+      if (rows[i].group == name) {
+        rows[i].count++;
+      }
+    }
+    console.log(rows);
+  }
+}
