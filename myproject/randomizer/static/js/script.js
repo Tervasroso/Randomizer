fcae8f8d033afd40bc1b62873bb51b5c737651cd
Tervasroso <tuokivilahti@gmail.com>
2026-01-29 11:13:37 +0200

    all chosen gamemodes now trigger html to page accordingly

--- myproject/randomizer/static/js/script.js
+++ myproject/randomizer/static/js/script.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Page loaded");
+  document.getElementById("selectFirst").innerHTML = `<form>
+    <input type="checkbox" id="selfirst">
+    <label for="selfirst">7+0</label>
+    </form>`;
 });
+
+Järjestelmä = `<form>
+    <input type="checkbox" id="selfirst">
+    <label for="selfirst">7+0</label>
+    </form>`;
+
+Harava1 = `<form>
+  <input type="checkbox" id="selfirst">
+  <label for="selfirst">7+1</label>
+  </form>`;
+
+Harava2 = `<form>
+  <input type="checkbox" id="selfirst">
+  <label for="selfirst">7+2</label>
+  </form>`;
 
 ov = 0;
 tv = 0;
@@ -59,9 +78,32 @@ function ifChecked(id, name) {
   }
 }
 
-function renderSelections(id) {
-  document.getElementById(id).innerHTML = '<form action="">';
-  ('<input type="checkbox" id="selfirst">');
-  ('<label for="selfirst">7+0</label>');
-  ("</form>");
+function renderSystem() {
+  console.log("Pöö");
+  document.getElementById(id).innerHTML = `<form>
+  <input type="checkbox" id="selfirst">
+  <label for="selfirst">7+0</label>
+  </form>`;
+}
+
+function renderRakeOne() {
+  console.log("Pöö");
+  // document.getElementById("selectFirst").innerHTML = `<form>
+  // <input type="checkbox" id="rakeOne">
+  // <label for="selfirst">7+1</label>
+  // </form>`;
+  document.getElementById("selectFirst").innerHTML = "Tässäpä muutos";
+}
+function myFunction() {
+  x = document.getElementById("gameMode").value;
+  console.log(x);
+  if (x == "Harava1") {
+    document.getElementById("selectFirst").innerHTML = Harava1;
+  }
+  if (x == "Harava2") {
+    document.getElementById("selectFirst").innerHTML = Harava2;
+  }
+  if (x == "Järjestelmä") {
+    document.getElementById("selectFirst").innerHTML = Järjestelmä;
+  }
 }
